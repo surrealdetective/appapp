15eac41bac06ee295dbd2b04f7f4c804fcf31f10
Max Jacobson <maxwell.jacobson@gmail.com>
2013-07-15 12:22:57 -0400

    scaffolded users, drafted seedsrb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+joe = User.new
+joe.name = "Joe Giralt"
+joe.email = "joegiralt@gmail.com"
+
+joe.save_user #update the status
+
+joe.application = Application.new
+# joe.current_status = "created" by default
+# user has_one application (??)
+joe.user_attributes.build(:tagline => "From there to here")
+joe_app.save_app #this updates the status again
+
+#joe.statuses.build(:name => "submitted")
+#joe_app.save
