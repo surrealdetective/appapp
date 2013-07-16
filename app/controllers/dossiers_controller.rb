9f55487a5657438dd985934bc233c7d03328a488
Joseph Giralt <joe.m.giralt@gmail.com>
2013-07-16 14:45:50 -0400

    adds dossier form functionality and redirects to dossier form ergnomically

--- app/controllers/dossiers_controller.rb
+++ app/controllers/dossiers_controller.rb
@@ -1,10 +1,19 @@
 class DossiersController < ApplicationController
 
   def new
+    @user= User.find(params[:user_id])
+    #raise @user.inspect
+
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @user.dossiers.build(params[:dossier])
+    @user.save
+    
+    redirect_to @user
+
   end
 
-
+  
 end
