a88d1ac3414d506577f438d5556468d85d38ec1a
Max Jacobson <maxwell.jacobson@gmail.com>
2013-07-22 15:56:14 -0400

    adds passwords to seeds.db and changes user_path link to dossier_path in dossier#show

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@
 joe = User.new
 joe.name = "Joe Giralt"
 joe.email = "joegiralt@gmail.com"
+joe.password = 'armyman'
 joe.save #update the status
 # joes_dossier = Dossier.new
 # joes_dossier.tagline = "I need a better tagline!"
@@ -20,11 +21,13 @@ max = User.new
 max.name = "Max Jacobson"
 max.email = "maxjacobson@maxwhatever.com"
 max.dossiers.build(:tagline => "Goofy's kid")
+max.password = 'macman'
 max.save_with_dossier_status("submitted")
 
 alex = User.new
 alex.name = "Alex Au"
 alex.email = "alex@alexoo.com"
+alex.password = 'myman'
 alexs_dossier = Dossier.new
 alexs_dossier.tagline = "get lucky"
 alex.dossiers << alexs_dossier
@@ -33,6 +36,7 @@ alex.save_with_dossier_status("submitted")
 sam = User.new
 sam.name = "Sam Rad"
 sam.email = "sam@sammo.com"
+sam.password = 'sammy'
 sam.dossiers.build(tagline: "Stun me")
 sam.save_with_dossier_status("submitted")
 
@@ -40,12 +44,14 @@ sam.save_with_dossier_status("submitted")
 miley = User.new
 miley.name = "Miley Cyrus"
 miley.email = "miley@twerk.com"
+miley.password = 'lights'
 miley.dossiers.build(tagline: "Just can't stop")
 miley.save_with_dossier_status("submitted")
 
 dhh = User.new
 dhh.name = "DHH"
 dhh.email = "dhh@icreatedrails.com"
+dhh.password = 'roarrr'
 dhh.dossiers.build(tagline: "Racecars")
 dhh.save_with_dossier_status("submitted")
 
@@ -62,3 +68,5 @@ sam.save_with_dossier_status("accepted")
 max.save_with_dossier_status("deferred")
 dhh.save_with_dossier_status("declined")
 
+100.times do
+end
