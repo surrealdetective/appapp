a905b24dc7dbf7767b2f3460446beb800c87d82e
surrealdetective <thefifth@gmail.com>
2013-07-24 14:42:11 -0400

    adds @thinner to thinner#index to show many dossiers

--- app/controllers/thinner_controller.rb
+++ app/controllers/thinner_controller.rb
@@ -1,6 +1,8 @@
 class ThinnerController < ApplicationController
+  layout "thinner"
+  
   def index
-    # @thinning = Users.where(:status => "")
+    @thinner = Dossier.where(:aasm_state => "new").limit(5)
     @dossier = Dossier.where(:id => 5).first
   end
 
