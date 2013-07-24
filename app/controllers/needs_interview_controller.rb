d12a407eb9e69e6c728ad42f020aca9ab4ede952
Joseph Giralt <joe.m.giralt@gmail.com>
2013-07-24 17:31:14 -0400

    nneeds to stop to present shit may break

--- app/controllers/needs_interview_controller.rb
+++ app/controllers/needs_interview_controller.rb
@@ -8,6 +8,11 @@ class NeedsInterviewController < ApplicationController
  def index
    @dossier = Dossier.find(params[:id])
    @user = @dossier.user
+   if params[:search]
+      @dossiers = Dossier.joins(:user).where(:users => {:first_name => params[:search]})
+    else
+      @dossiers = Dossier.sort_by(params[:sort_by])
+    end
  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 classes = ["Fall 2013", "Spring 2014", "Summer 2014"]
 
-rand(400).times do
+400.times do
   user = User.new
   user.first_name = Populator.words(1)
   user.last_name = Populator.words(1)
