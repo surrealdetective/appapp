1cc92bb508184db59f31054496575fabee2b3902
surrealdetective <thefifth@gmail.com>
2013-07-31 18:13:20 -0400

    shows interview time on needs interview page

--- app/controllers/needs_interview_controller.rb
+++ app/controllers/needs_interview_controller.rb
@@ -1,8 +1,11 @@
 class NeedsInterviewController < ApplicationController
   def index
-    #order the dossier methods by upcoming interviews.
-    @dossiers = Dossier.joins(:interviews).where('dossiers.aasm_state' => ["needs_interview", "needs_code_interview"]).order('interviews.interview_time')
-    # @dossiers = Dossier.where(:aasm_state => ["needs_interview", "needs_code_interview"])
+    #consider making a reads_interview method for dossier, which will protect us from nilling out.
+    @dossiers_with_interviews = Dossier.joins(:interviews).where('dossiers.aasm_state' => ["needs_interview", "needs_code_interview"]).order('interviews.interview_time')
+    @dossiers_without_interviews = Dossier.where(:aasm_state => ["needs_interview", "needs_code_interview"])
+    @dossiers = (@dossiers_with_interviews + @dossiers_without_interviews).uniq
     authorize! :index, @dossiers
   end
+
+
 end
