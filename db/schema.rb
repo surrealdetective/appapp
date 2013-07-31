351afb1bf07e67cfdb4906387094df4dc758e6cf
Joseph Giralt <joe.m.giralt@gmail.com>
2013-07-31 19:22:16 -0400

    slight fix in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20130731190939) do
-
+ActiveRecord::Schema.define(:version => 20130731195313) do
 
   create_table "courses", :force => true do |t|
     t.string   "subject"
