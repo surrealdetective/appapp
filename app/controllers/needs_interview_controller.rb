5424683d5b7b13e16fe123ffab9fb44a074af8ec
surrealdetective <thefifth@gmail.com>
2013-08-01 14:53:09 -0400

    transports search sort_by function to needs_interview controller

--- app/controllers/needs_interview_controller.rb
+++ app/controllers/needs_interview_controller.rb
@@ -1,10 +1,20 @@
 class NeedsInterviewController < ApplicationController
   def index
     #consider making a reads_interview method for dossier, which will protect us from nilling out.
-    @dossiers_with_interviews = Dossier.joins(:interviews).where('dossiers.aasm_state' => ["needs_interview", "needs_code_interview"]).order('interviews.interview_time')
-    @dossiers_without_interviews = Dossier.where(:aasm_state => ["needs_interview", "needs_code_interview"])
-    @dossiers = (@dossiers_with_interviews + @dossiers_without_interviews).uniq
-    authorize! :index, @dossiers
+    
+    #what we want is below == @dossiers = Dossier.sort_by(params[:sort_by])
+
+    if params[:sort_by] #some condition
+      @dossiers_with_interviews = Dossier.joins(:interviews).where('dossiers.aasm_state' => ["needs_interview", "needs_code_interview"]).order('interviews.interview_time').sort_by(params[:sort_by])
+      @dossiers_without_interviews = Dossier.where(:aasm_state => ["needs_interview", "needs_code_interview"]).sort_by(params[:sort_by])
+      @dossiers = (@dossiers_with_interviews + @dossiers_without_interviews).uniq
+    else
+      #what is below: == @dossiers = Dossier.find(:all)
+      @dossiers_with_interviews = Dossier.joins(:interviews).where('dossiers.aasm_state' => ["needs_interview", "needs_code_interview"]).order('interviews.interview_time')
+      @dossiers_without_interviews = Dossier.where(:aasm_state => ["needs_interview", "needs_code_interview"])
+      @dossiers = (@dossiers_with_interviews + @dossiers_without_interviews).uniq
+    end
+    authorize! :index, @dossiers    
   end
 
 
