6909f033d9c300af8c44167872897674b414846c
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-04 19:11:38 -0400

    removes pretty layout and updates welcome controller to redirect to new dossier path

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,26 +1,23 @@
 class WelcomeController < ApplicationController
-  skip_before_filter :login_required
-  layout "pretty"
 
-  # lot of goofy conditionals here
-  # rough draft
-  
+  # you don't need to be logged in for this controller
+  skip_before_filter :login_required
+
   def home
 
     if current_user
-      if current_user.role?(:moderator)
+      if current_user.admin?
         redirect_to dashboard_path
       else
-        if current_user.last_dossier
+        if current_user.has_dossiers?
           redirect_to current_user.last_dossier
         else
           redirect_to new_dossier_path
         end
       end
+    else
+      redirect_to new_dossier_path
     end
-
-    @title = "Welcome"
-    @body_classes = "login-wrapper"
 
   end
 end
