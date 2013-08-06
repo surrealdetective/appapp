e797a70550a3b6c88ef386c229d4b3ff258a7fc2
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-06 16:00:50 -0400

    renames courses_dashboard_path to dashboard_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Appapp::Application.routes.draw do
   
   resources :courses, :only => [:new, :create, :index]
-  get "/dashboard" => "courses#dashboard", :as => "courses_dashboard"
+  get "/dashboard" => "courses#dashboard", :as => "dashboard"
 
   get "/class" => "class#index"
   post "/class/:dossier_id/decision" => "class#decision", :as=> "class_decision"
