7a4158772fbca5cb517216fbf4baba856fd16d81
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-07 10:14:49 -0400

    adds ability to edit your own dossier IF AND ONLY IF its new

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,9 @@ class Ability
     else
       can :create, Dossier
       can :create, User
+      can :update, Dossier do |dossier|
+        dossier.user_id == user.id && dossier.aasm_state == "new"
+      end
       can :read, Dossier, :user_id => user.id
     end
   end
