1a73bf8c62206d11fa87ec9b414e2cc192c4b60a
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-08 12:01:15 -0400

    only runs morris charts when their target div is present

--- app/assets/javascripts/barchart.js
+++ app/assets/javascripts/barchart.js
@@ -3,29 +3,34 @@ $(document).ready(function() {
    * Play with this code and it'll update in the panel opposite.
    *
    * Why not try some of the options above?
-   */
-   var viewed_today = $("#bar-example").data("viewed_today");
-   var viewed_yesterday = $("#bar-example").data("viewed_yesterday");
+  */
 
-   var reviewed_today = $("#bar-example").data("reviewed_today");
-   var reviewed_yesterday = $("#bar-example").data("reviewed_yesterday");
 
-   var interviewed_today = $("#bar-example").data("interviewed_today");
-   var interviewed_yesterday = $("#bar-example").data("interviewed_yesterday");
+  if($("#bar-example").length > 0) {
+    var viewed_today = $("#bar-example").data("viewed_today");
+    var viewed_yesterday = $("#bar-example").data("viewed_yesterday");
 
-   var resolved_today = $("#bar-example").data("resolved_today");
-   var resolved_yesterday = $("#bar-example").data("resolved_yesterday");
+    var reviewed_today = $("#bar-example").data("reviewed_today");
+    var reviewed_yesterday = $("#bar-example").data("reviewed_yesterday");
 
-  Morris.Bar({
-    element: 'bar-example',
-    data: [
-      { y: 'Viewed', a: viewed_today, b: viewed_yesterday },
-      { y: 'Reviewed', a: reviewed_today, b: reviewed_yesterday },
-      { y: 'Interviewed', a: interviewed_today, b: interviewed_yesterday },
-      { y: 'Resolved', a: resolved_today, b: resolved_yesterday }
-    ],
-    xkey: 'y',
-    ykeys: ['a', 'b'],
-    labels: ['Actions Today', 'Actions Yesterday']
-  });
+    var interviewed_today = $("#bar-example").data("interviewed_today");
+    var interviewed_yesterday = $("#bar-example").data("interviewed_yesterday");
+
+    var resolved_today = $("#bar-example").data("resolved_today");
+    var resolved_yesterday = $("#bar-example").data("resolved_yesterday");
+
+    Morris.Bar({
+      element: 'bar-example',
+      data: [
+        { y: 'Viewed', a: viewed_today, b: viewed_yesterday },
+        { y: 'Reviewed', a: reviewed_today, b: reviewed_yesterday },
+        { y: 'Interviewed', a: interviewed_today, b: interviewed_yesterday },
+        { y: 'Resolved', a: resolved_today, b: resolved_yesterday }
+      ],
+      xkey: 'y',
+      ykeys: ['a', 'b'],
+      labels: ['Actions Today', 'Actions Yesterday']
+    });
+  }
+
 });
--- app/assets/javascripts/donut.js
+++ app/assets/javascripts/donut.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  
-  var male_count = $("#donut-example").data("male");
-  var female_count = $("#donut-example").data("female");
-  var other_count = $("#donut-example").data("other");
+  if($("#donut-example").length > 0) {
+    var male_count = $("#donut-example").data("male");
+    var female_count = $("#donut-example").data("female");
+    var other_count = $("#donut-example").data("other");
 
-  Morris.Donut({
-    element: 'donut-example',
-    data: [
-      {label: "Male", value: male_count},
-      {label: "Female", value: female_count},
-      {label: "Other", value: other_count}
-    ]
-  });
-  
+    Morris.Donut({
+      element: 'donut-example',
+      data: [
+        {label: "Male", value: male_count},
+        {label: "Female", value: female_count},
+        {label: "Other", value: other_count}
+      ]
+    });
+  }
 });
