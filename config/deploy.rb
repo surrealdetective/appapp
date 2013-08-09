64bad60b32a52d12b9b694e6d31820d334fae0bf
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-09 11:36:12 -0400

    rewrites config/deploy with custom info

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,18 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano' # for bundler support
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "appapp"
+set :repository,  "git@github.com:flatiron-school/appapp.git"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :user, 'deploy'
+set :deploy_to, "/home/#{ user }/#{ application }"
+set :use_sudo, false
+
+set :scm, :git
+
+default_run_options[:pty] = true
+
+role :web, "192.241.176.139"
+role :app, "192.241.176.139"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -16,10 +21,10 @@ role :db,  "your slave db-server here"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+end
