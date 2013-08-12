7bf611dbb05758ffd04a52004e707a5b0cc0bb81
surrealdetective <thefifth@gmail.com>
2013-08-12 13:20:22 -0400

    adjusts character limit

--- app/helpers/thinner_helper.rb
+++ app/helpers/thinner_helper.rb
@@ -1,6 +1,10 @@
 module ThinnerHelper
   def four_hundred_twenty_chars(str)
-    str[0..419]
+    if str.length >= 417
+      str[0..415] + "..."
+    else
+      str[0..417]
+    end
   end
 
   def one_hundred_forty_chars(str)
