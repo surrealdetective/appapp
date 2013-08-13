92f626e1f4480a453bb9b3fe3078df9961f2e7af
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-13 19:41:26 -0400

    updates deploy.rb with symlink for persistent production database

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,8 @@ default_run_options[:pty] = true
 role :web, "192.241.176.139"
 role :app, "192.241.176.139"
 
+before 'deploy:assets:precompile', 'deploy:symlink_config'
+
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
@@ -26,5 +28,8 @@ namespace :deploy do
  task :stop do ; end
  task :restart, :roles => :app, :except => { :no_release => true } do
    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+ task :symlink_config, :roles => :app do 
+   run "ln -nfs #{shared_path}/production.sqlite3 #{current_release}/db/production.sqlite3"
  end
 end
