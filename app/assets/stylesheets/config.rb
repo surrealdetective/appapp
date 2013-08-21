a082adb4a17fc0772fa9cb37bdc7cbcb93de8bdb
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-21 14:46:21 -0400

    refactors css slightly

--- app/assets/stylesheets/config.rb
+++
@@ -1,9 +0,0 @@
-## commented out because I don't know what this is for
-# http_path = "/"
-# css_dir = "."
-# sass_dir = "."
-# images_dir = "img"
-# javascripts_dir = "js"
-# output_style = :compressed
-# relative_assets=true
-# line_comments = false
